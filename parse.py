d8ca1ceb7b2e9caae9cd4a370de300194032f7cb
Alec Mikell <alecdeveloper@gmail.com>
2018-02-21 12:29:02 -0500

    parses through all messages, planning on HDF store

--- parse.py
+++ parse.py
@@ -1,4 +1,21 @@
+import numpy as np
 import pandas as pd
-from bs4 import BeautifulSoup as bs
+import datetime
+from bs4 import BeautifulSoup
 
-print("This is a Python file.")
+messages_index=BeautifulSoup(open("messages/messages.htm"),"html5lib")
+print("Opened messages/mesages.htm")
+
+df=pd.DataFrame([], columns=['friend', 'date', 'contents'], index=[])
+count=0
+
+for a in messages_index.find_all('a', href=True):
+    if(a['href'][:2]=="me"):##message file, parse it
+        message_file=BeautifulSoup(open(a['href']),"html5lib")
+        print(a['href'])
+        friend=
+        for message in message_file.find_all("div", {"class": "message"}):
+            df.append
+            count=count+1
+
+print(count)
